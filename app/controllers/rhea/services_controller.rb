8e8c36e677ea5eb131757a4b1b8a3c03b727c0c8
Tom Benner <tombenner@gmail.com>
2015-09-18 16:58:37 -0700

    Add more service links

--- app/controllers/rhea/services_controller.rb
+++ app/controllers/rhea/services_controller.rb
@@ -1,10 +1,15 @@
 module Rhea
   class ServicesController < Rhea::BaseController
     def index
-      services_url = "#{Rhea.settings[:kube_api][:url]}v1/proxy/namespaces/kube-system/services/"
+      api_url = Rhea.settings[:kube_api][:url]
+      root_url = api_url.sub('/api/', '/')
+      services_url = "#{api_url}v1/proxy/namespaces/kube-system/services/"
       @services = {
         'Kubernetes UI' => "#{services_url}kube-ui/",
-        'Grafana' => "#{services_url}monitoring-grafana/"
+        'Grafana' => "#{services_url}monitoring-grafana/",
+        'Kibana' => "#{services_url}kibana-logging/",
+        'Logs' => "#{services_url}logs/",
+        'API Documentation' => "#{root_url}swagger-ui/"
       }
     end
   end
