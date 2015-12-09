096fe79407cdcb171d7555bd5cb900225f8a9b43
Tom Benner <tombenner@gmail.com>
2015-12-08 16:51:48 -0800

    Label values must be 63 characters or fewer

--- lib/rhea/kubernetes/commands/base.rb
+++ lib/rhea/kubernetes/commands/base.rb
@@ -15,7 +15,7 @@ module Rhea
           command_hash = Digest::MD5.hexdigest("#{image}#{command_expression}")[0..3]
           command_for_host = command_expression.downcase.gsub(/[^-a-z0-9]+/i, '-').squeeze('-')
           key = "#{key_prefix}#{command_hash}-#{command_for_host}"
-          max_host_name_length = 64
+          max_host_name_length = 62
           key = key[0,max_host_name_length]
           # The key can't end with a '-'
           key.gsub!(/\-+$/, '')
